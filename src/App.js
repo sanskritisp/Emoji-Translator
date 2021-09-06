de8f70784411e19e49f1a9e6056d31752239e192
SanskritiPandey <khushiksp17@gmail.com>
2021-09-06 13:59:09 -0700

    feat :completed the code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./style.css";
+import { useState } from "react";
+import {emojiDictionary} from "./dict";
+
+
+
+var emojiWeHave = Object.keys(emojiDictionary);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [meaning, setMeaning] = useState("");
+  function emojiInputHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    if (meaning === undefined) {
+      meaning = emoji + "we do not have this emoji in our database";
+    }
+    else{
+      meaning = emoji+ "  means : "+ meaning;
+    }
+    setMeaning(meaning);
+  }
+
+return (
+     <div className="App">
+      <h1>EMOJI TRANSLATOR</h1>
+      <h2>Enter the emoji to see it's meaning</h2>
+
+      <input onChange={(e) => emojiInputHandler(e.target.value)}></input>
+      <h2>{meaning}</h2>
+      <h3>Emoji that we have:</h3>
+      <div>
+        {" "}
+        {emojiWeHave.map((emoji) => {
+          return (
+            <span
+              onClick={() => {
+                emojiInputHandler(emoji);
+              }}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
+
 
 export default App;
